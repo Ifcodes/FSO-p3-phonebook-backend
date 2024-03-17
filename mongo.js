d3294060a0790a26d4928a1e3b44b7ce42c6202a
Desmond <ifeco133@gmail.com>
2024-03-17 12:04:42 +0000

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Please provide password");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://bernarddesmond16:${password}@cluster0.f52pck0.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const phonebookSchema = mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Contact = mongoose.model("Contact", phonebookSchema);
+
+const contact = new Contact({
+  name: "Desmond",
+  number: "888-333-3332",
+});
+
+contact.save().then((result) => {
+  console.log(`added ${result.name} number ${result.number} to phonebook`);
+  mongoose.connection.close();
+});
